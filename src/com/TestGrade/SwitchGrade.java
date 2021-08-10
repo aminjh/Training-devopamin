3d2ba0e2018f4da8081c98b75250bd03e2605545
amine <jhaidriamin@gmal.com>
2021-08-10 10:06:54 -0500

    Added new file

---
+++ src/com/TestGrade/SwitchGrade.java
@@ -0,0 +1,19 @@
+package com.TestGrade;
+
+public class SwitchGrade {
+
+	public static void main(String[] args) {
+		int n =70;
+		switch (n) {
+		case 70 :
+			System.out.println ("D");
+		case 80:
+			System.out.println ("B");
+		case 90:
+			System.out.println ("A");
+			default:
+				break;
+	}
+
+}
+}
